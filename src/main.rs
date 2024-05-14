8606aa190a5dceb7e2e6afd06d7ac5071caaa283
leinweberm <leinweber.martin@gmail.com>
2024-05-14 22:21:15 +0200

    [wip] tokio spawn tasks

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod utils;
 
 #[tokio::main]
 async fn main() {
+    // Routes init
     let routes = requests::router::router();
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
--- src/requests/routes/test/upload.rs
+++ src/requests/routes/test/upload.rs
@@ -1,37 +1,55 @@
 use warp::{ Filter, Rejection, Reply };
-// use bytes::BufMut;
 use futures_util::TryStreamExt;
 use warp::multipart::FormData;
 use warp::Buf;
 use warp::http::{ Response, StatusCode };
 use sha2::{ Sha256, Digest };
+use std::sync::{Arc, Mutex};
+use tokio::task;
+use std;
 
 async fn post_upload(form: FormData) -> Result<impl Reply, Rejection> {
     let result: Vec<_> = form
         .and_then(| mut field | async move {
-            // let mut bytes: Vec<u8> = Vec::new();
             let mut hasher = Sha256::new();
-            println!("field content-type: {:?}", field.content_type());
-            println!("field name: {:?}", field.name());
+            let mut size: usize = 0;
 
-            if let Some(filename) = field.filename() {
-                println!("filename: {:?}", filename);
+            let mut content_type = String::from("form-data/field");
+            if field.content_type().is_some() {
+                content_type = String::from(format!("{:?}", field.content_type()));
             }
 
-            // field.data() only returns a piece of the content, you should call over it until it replies None
             while let Some(content) = field.data().await {
-                let content = content.unwrap();
-                println!("Content: {:?}", content.chunk().len());
-                hasher.update(content.chunk());
-                // bytes.put(content);
+                // println!("Content: {:?}", content.chunk().len());
+                // size += content.chunk().len();
+                // hasher.update(content.chunk());
+
+                let part = content.unwrap();
+                let chunk = Arc::new(Mutex::new(part.chunk()));
+                let thread_1_chunk = Arc::clone(&chunk);
+                let thread_2_chunk = Arc::clone(&chunk);
+
+                let size_task = task::spawn(async {
+                    let data = thread_1_chunk.lock().unwrap();
+                    size += data.len();
+                    std::thread::sleep(std::time::Duration::from_micros(500_000));
+                });
+
+                let hash_task = task::spawn(async {
+                    let data = thread_2_chunk.lock().unwrap();
+                    std::thread::sleep(std::time::Duration::from_micros(500_000));
+                });
+
+                size_task.await.unwrap();
+                hash_task.await.unwrap();
             }
 
             let hash = hasher.finalize();
 
             Ok((
-                // field.filename().unwrap().to_string(),
+                format!("Content-Type: {}", content_type),
                 format!("{:x}", hash),
-                // String::from_utf8_lossy(&*bytes).to_string(),
+                format!("{} MB", (&size / 1024 / 1024).to_string())
             ))
         })
         .try_collect()
