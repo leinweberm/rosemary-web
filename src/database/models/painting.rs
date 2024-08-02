15f13b77bb451b3752f29227d97b547c70cd4a33
Martin Leinweber <martin.leinweber@bistudio.com>
2024-08-02 17:00:07 +0200

    [wip] new database connection handler

--- src/database/models/painting.rs
+++ src/database/models/painting.rs
@@ -2,10 +2,11 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use std::collections::HashMap;
+use sqlx::{postgres::PgRow, Error, FromRow, Row};
 
 use crate::database::models::generics::Translation;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Painting {
     pub id: Uuid,
     pub created: DateTime<Utc>,
@@ -19,6 +20,34 @@ pub struct Painting {
     pub preview: PaintingImage,
 }
 
+impl <'r>FromRow<'r, PgRow> for Painting {
+    fn from_row(row: &'r PgRow) -> Result<Self, Error> {
+        let id = row.try_get("id")?;
+        let created = row.try_get("created")?;
+        let deleted = row.try_get("deleted")?;
+        let price = row.try_get("price")?;
+        let painting_title = row.try_get("painting_title")?;
+        let painting_description = row.try_get("painting_description")?;
+        let data = row.try_get("data")?;
+        let width = row.try_get("width")?;
+        let height = row.try_get("height")?;
+        let preview = row.try_get("preview")?;
+
+        Ok(Painting{
+            id,
+            created,
+            deleted,
+            price,
+            painting_title,
+            painting_description,
+            data,
+            width,
+            height,
+            preview
+        })
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PaintingImage {
     pub id: Uuid,
@@ -27,4 +56,24 @@ pub struct PaintingImage {
     pub alt: Option<String>,
     pub title: Option<String>,
     pub painting_id: Uuid,
+}
+
+impl <'r>FromRof<'r, PgRow> for PaintingImage {
+    fn from_row(row: &'r PgRow) -> Result<Self, Error> {
+        let id = row.try_get("id")?;
+        let preview = row.try_get("preview")?;
+        let url = row.try_get("url")?;
+        let alt = row.try_get("alt")?;
+        let title = row.try_get("titke")?;
+        let painting_id = row.try_get("painting_id")?;
+
+        Ok(PaintingImage{
+            id,
+            preview,
+            url,
+            alt,
+            title,
+            painting_id
+        })
+    }
 }
--- src/requests/routes/v1_0/paintings/get_all.rs
+++ src/requests/routes/v1_0/paintings/get_all.rs
@@ -1,127 +1,59 @@
-use postgres::types::ToSql;
-use uuid::Uuid;
 use warp::{Filter, Rejection, Reply, path, query};
 
-use crate::database::models::generics::Translation;
 use crate::requests::dto::get_paintings_query::GetPaintingsQuery;
-use crate::requests::dto::paginated_result::PaginatedResult;
+use crate::database::models::painting::Painting;
 use crate::database::connection::get_client;
-use crate::database::models::painting::{Painting, PaintingImage};
+use crate::requests::dto::paginated_result::PaginatedResult;
 
-async fn get_paintings(query: GetPaintingsQuery) -> Result<impl Reply, Rejection> {
+async fn get_paintings(query: GetPaintingsQuery) {
     let mut result: PaginatedResult<Painting> = PaginatedResult::new();
-
     let client: &tokio_postgres::Client = get_client().await.unwrap();
 
-    let limit = query.limit.unwrap_or(20);
-    let offset = query.offset.unwrap_or(0);
-    let order = query.order.unwrap_or("DESC".to_string());
-    let sort = query.sort.unwrap_or("created".to_string());
-    let search = query.search.unwrap_or("".to_string());
 
-    let search_clone = search.clone();
-
-    let count_query = tokio::spawn(async move {
-        let mut query_string = String::from("SELECT COUNT(*) FROM paintings");
-        let mut params = vec![];
-        let search_param;
-
-        if &search_clone != "" {
-            query_string.push_str(" WHERE painting_title ILIKE $1
-            OR painting_description ILIKE $1 OR ");
-            search_param = format!("%{}%", &search_clone);
-            params.push(&search_param as &(dyn ToSql + Sync));
-        }
-
-        let rows = client
-            .query(&query_string, &params)
-            .await
-            .unwrap();
-
-        let count: i64 = rows[0].get(0);
+    let count_task = tokio::spawn(async move {
+        let count = sqlx::query(r#"
+            SELECT COUNT(*)
+            FROM paintings
+            WHERE deleted IS NULL
+        "#)
+            .fetch_one(&client)
+            .await?;
         count
     });
 
-    let rows_query = tokio::spawn(async move {
-        let mut params = vec![
-            &limit as &(dyn ToSql + Sync),
-            &offset as &(dyn ToSql + Sync),
-            &sort as &(dyn ToSql + Sync),
-            &order as &(dyn ToSql + Sync),
-        ];
 
-        let mut search_query =String::from("");
-
-        if search != "" {
-            params.push(&format!("%{}%", search) as &(dyn ToSql + Sync));
-            search_query = String::from("AND (p.painting_title ILIKE $5 OR p.painting_description ILIKE $5)");
-        }
-
-        let mut query_string = String::from(format!(r#"
+    let rows_task = tokio::spawn(async move {
+        let rows = sqlx::query_as::<_, Painting>(r#"
             SELECT
-                p.*,
-                pi.id AS preview_id,
-                pi.preview AS preview_preview,
-                pi.url AS preview_url,
-                pi.alt AS preview_alt,
-                pi.title AS preview_title,
-                pi.painting_id AS preview_painting_id
+                p.id,
+                p.created,
+                p.deleted,
+                p.price,
+                p.painting_title,
+                p.painting_description,
+                p.data,
+                p.width,
+                p.height,
+                p.preview
             FROM paintings p
-            LEFT JOIN painting_images AS pi ON pi.painting_id = p.id AND pi.preview = true
-            WHERE p.deleted IS NULL
-            {}
-            LIMIT $1 OFFSET $2
-            ORDER BY $3 $4
-
-        "#, &search_query));
-
-        let rows = client
-            .query(&query_string, &params)
-            .await
-            .unwrap();
-
-        let result = rows
-            .iter()
-            .map(|row| {
-                let painting_title_value: serde_json::Value = row.get("painting_title");
-                let painting_title: Option<Translation> = serde_json::from_value(painting_title_value).unwrap();
-
-                Painting {
-                    id: row.get("id") as Uuid,
-                    created: row.get("created"),
-                    deleted: row.get("deleted"),
-                    price: row.get("price"),
-                    painting_title,
-                    painting_description: row.get("painting_description"),
-                    data: row.get("data"),
-                    width: row.get("width"),
-                    height: row.get("height"),
-                    preview: PaintingImage {
-                        id: row.get("preview_id") as Uuid,
-                        preview: row.get("preview_preview"),
-                        url: row.get("preview_url"),
-                        alt: row.get("preview_alt"),
-                        title: row.get("preview_title"),
-                        painting_id: row.get("preview_painting_id") as Uuid,
-                    },
-                }
-            })
-            .collect::<Vec<Painting>>();
-
-        println!("paintings: {:?}", &result);
-
-        result
+            WHERE deleted IS NULL
+            ORDER BY $1 $2
+            LIMIT $3 OFFSET $4
+        "#)
+            .bind(query.sort.unwrap_or("created".to_string()))
+            .bind(query.order.unwrap_or("DESC".to_string()))
+            .bind(query.limit.unwrap_or(20) as f32)
+            .bind(query.offset.unwrap_or(0) as f32)
+            .fetch_all(&client);
+            // .await?;
+        rows
     });
 
-    let (count, rows) = tokio::try_join!(count_query, rows_query).unwrap();
+    let (count, rows) = tokio::try_join!(count_task, rows_task).unwrap();
+    println!("Count: {}", &count);
+    println!("Rows: {}", &rows);
 
-    if !rows.is_empty() {
-        result.rows = rows;
-    }
-
-    result.count = count;
-
-    Ok(warp::reply::json(&result))
+    Ok(warp::reply::json(&result));
 }
 
 pub fn get() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
