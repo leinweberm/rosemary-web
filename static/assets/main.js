bf559ac55534fb9d1ab4afdf05e7b934f5117a07
Martin Leinweber <martin.leinweber@bistudio.com>
2025-01-02 23:57:42 +0100

    [fix] navbar template styles

--- static/assets/main.js
+++ static/assets/main.js
@@ -2,22 +2,40 @@ let lastScroll = 0;
 let navHidden = false;
 let isNavBig = true;
 
-const prepareNavbarCheckbox = () => {
-	const checkbox = document.querySelector('#navToggleCheckbox');
-	checkbox && checkbox.addEventListener('change', (event) => {
-		const nav = document.querySelector('#nav');
-		const { checked } = event.target;
-		nav.style.top = '0';
-		document.body.style.height = (checked) ? '100dvh' : 'unset';
-		document.body.style.overflow = (checked) ? 'hidden' : 'unset';
-	});
+const debounce = (func, delay) => {
+	let timeout;
+	return (...args) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => func(...args), delay);
+	};
+}
+
+const throttle = (func, limit) => {
+	let lastFunc;
+	let lastRan;
+	return (...args) => {
+		const context = this;
+		const now = Date.now();
+
+		if (!lastRan) {
+			func.apply(context, args);
+			lastRan = now;
+		} else {
+			clearTimeout(lastFunc);
+			lastFunc = setTimeout(() => {
+				if (now - lastRan >= limit) {
+					func.apply(context, args);
+					lastRan = now;
+				}
+			}, limit - (now - lastRan));
+		}
+	};
 };
 
 const prepareNavbarPosition = () => {
 	const nav = document.querySelector('#nav');
-	nav && window.addEventListener('scroll', () => {
+	nav && window.addEventListener('scroll', throttle(() => {
 		let scroll = Math.round(window.scrollY);
-		console.log('scroll', scroll);
 		if (scroll > lastScroll && isNavBig) {
 			nav.classList.add('navSmall');
 			isNavBig = false;
@@ -26,11 +44,10 @@ const prepareNavbarPosition = () => {
 			isNavBig = true;
 		}
 		lastScroll = scroll;
-	});
+	}, 200));
 };
 
 const addEventListeners = () => {
-	prepareNavbarCheckbox();
 	prepareNavbarPosition();
 };
 
