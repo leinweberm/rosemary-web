bf60f761abd7fd70aa78649a4d0704adf27068df
Leinweber Martin <leinweber.martin@gmail.com>
2025-04-02 23:46:08 +0200

    refactor(app): gallery detail tempalte
    finished html template of gallery item page

--- static/assets/galleryDetail.js
+++ static/assets/galleryDetail.js
@@ -1,9 +1,24 @@
 // @ts-check
 
-const addEventListeners = () => {
-	
+const imageSrcs = [];
+
+const openLargeImage = (event, src) => {
+	console.log(event.target.src);
+	console.log('src', src);
+}
+
+const initDetailPage = () => {
+	/** @type {NodeListOf<HTMLImageElement>} */
+	const images = document.querySelectorAll('.paintingImage');
+	console.log('images', images);
+	// images.forEach((image) => {
+		// console.log('image', image.src);
+		// if (image && image.src) {
+		// const baseSrc = image.src.replace(/_\d+\.jpg$/, '');
+		// }
+	// });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-
+	initDetailPage();
 });
