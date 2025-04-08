25e1681df1ed8a7798b87a913dd6c13bea35eb67
Leinweber Martin <leinweber.martin@gmail.com>
2025-04-08 23:41:32 +0200

    feat(app): painting image dialog
    display large version of image in modal dialog

--- static/assets/galleryDetail.js
+++ static/assets/galleryDetail.js
@@ -1,22 +1,74 @@
 // @ts-check
-
 const imageSrcs = [];
+let currentImageIndex = 0;
 
-const openLargeImage = (event, src) => {
-	console.log(event.target.src);
-	console.log('src', src);
-}
+/**
+ * @param {number} tempIndex -
+ * @return {void} imageSrc -
+ */
+const updateDialogImage = (tempIndex) => {
+	/** @type {HTMLImageElement | null} */
+	const dialogImage = document.querySelector('#imageDialogImage');
+	if (!dialogImage) return;
+	let finalIndex = 0;
+	const lastIndex = imageSrcs.length - 1;
+
+	if (tempIndex < 0) {
+		finalIndex = lastIndex;
+	} else if (tempIndex > lastIndex) {
+		finalIndex = 0;
+	} else {
+		finalIndex = tempIndex;
+	}
+
+	currentImageIndex = finalIndex;
+	let baseUrl = imageSrcs[finalIndex].replace(/_\d+\.jpg$/, '');
+	dialogImage.src=`${baseUrl}_${dialogImage.getAttribute('maxWidth')}.jpg`;
+};
 
 const initDetailPage = () => {
+	/** @type {HTMLDialogElement | null} */
+	const dialog = document.querySelector('#imageDialog');
+	/** @type {HTMLButtonElement | null} */
+	const closeButton = document.querySelector('#imageDialogClose');
+	/** @type {HTMLButtonElement | null} */
+	const prevButton = document.querySelector('#imageDialogPrev');
+	/** @type {HTMLButtonElement | null} */
+	const nextButton = document.querySelector('#imageDialogNext');
+	/** @type {HTMLImageElement | null} */
+	const dialogImage = document.querySelector('#imageDialogImage');
 	/** @type {NodeListOf<HTMLImageElement>} */
 	const images = document.querySelectorAll('.paintingImage');
-	console.log('images', images);
-	// images.forEach((image) => {
-		// console.log('image', image.src);
-		// if (image && image.src) {
-		// const baseSrc = image.src.replace(/_\d+\.jpg$/, '');
-		// }
-	// });
+
+	if (!dialog || !dialogImage) return;
+
+	(closeButton) && closeButton.addEventListener('click', () => {
+		dialog.close();
+	});
+	(dialog) && dialog.addEventListener('click', () => {
+		dialog.close();
+	});
+	(prevButton) && prevButton.addEventListener('click', (e) => {
+		e.stopPropagation();
+		updateDialogImage(currentImageIndex - 1);
+	});
+	(nextButton) && nextButton.addEventListener('click', (e) => {
+		e.stopPropagation();
+		updateDialogImage(currentImageIndex + 1);
+	});
+
+	images.forEach((image, index) => {
+		if (image && image.src) {
+			const baseSrc = image.src.replace(/_\d+\.jpg$/, '');
+			imageSrcs.push(baseSrc);
+
+			image.addEventListener('click', () => {
+				currentImageIndex = index;
+				dialogImage.src = `${baseSrc}_${dialogImage.getAttribute('maxWidth')}.jpg`;
+				dialog.showModal();
+			});
+		}
+	});
 };
 
 document.addEventListener('DOMContentLoaded', () => {
